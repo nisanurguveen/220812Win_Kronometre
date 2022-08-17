ca44ec1a020f0f506bb84bb925f8cc413cba403a
nisanurguveen <nisanurguveen@gmail.com>
2022-08-17 10:38:30 +0300

    Add project files.

---
+++ 220812Win_Kronometre/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace _220812Win_Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(158, 87);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(90, 32);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Location = new System.Drawing.Point(58, 39);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(24, 17);
+            this.lblSaat.TabIndex = 1;
+            this.lblSaat.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Location = new System.Drawing.Point(188, 39);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(24, 17);
+            this.lblDakika.TabIndex = 1;
+            this.lblDakika.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = ":";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Location = new System.Drawing.Point(320, 39);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(24, 17);
+            this.lblSaniye.TabIndex = 1;
+            this.lblSaniye.Text = "00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 148);
+            this.Controls.Add(this.lblSaniye);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDakika);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSaat);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 220812Win_Kronometre/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _220812Win_Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //aynı tipte biden fazla değişken tanımlanacaksa aralarına virgül konularak tanımlanabilir
+        int saat = 0, dakika = 0, saniye = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            if (saniye > 59)
+            {
+                dakika++;
+                if (dakika > 59)
+                {
+                    saat++;
+                    lblSaat.Text = dakika < 10 ? string.Format("0{0}", dakika) : dakika.ToString(); 
+                    dakika = 0;
+                }
+                lblDakika.Text = dakika < 10 ? string.Format("0{0}", dakika) : dakika.ToString();
+                saniye = 0;
+            }
+            lblSaniye.Text=saniye<10? string.Format("0{0}", saniye): saniye.ToString();
+            //TURNERY IF TEK SATIRDA IF YAZMA İŞLEMİ
+            
+
+
+        }
+    }
+}
